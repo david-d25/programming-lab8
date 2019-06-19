dccefe61a3dbab5364dd67dd1f9fe7d3c10786bf
David <david-d25@yandex.ru>
2019-06-19 03:15:54 +0300

    Removed shell scripts

--- clear.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-rm -r jars 2> /dev/null;
-rm -r build 2> /dev/null;
-rm -r javadoc 2> /dev/null;
--- free-ports.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-for i in {1..65535}; do (exec 2>&-; echo > /dev/tcp/localhost/$i && echo $i is open); done
